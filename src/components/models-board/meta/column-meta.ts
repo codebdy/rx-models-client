440d7d65fd201f4facf543614352710fd3edc488
Water.Li <lizongbin@hotmail.com>
2021-07-10 12:00:35 +0800

    backup

--- src/components/models-board/meta/column-meta.ts
+++ src/components/models-board/meta/column-meta.ts
@@ -9,4 +9,35 @@ export interface ColumnMeta{
   type: ColumnType;
   primary?: boolean;
   generated?: boolean;
+  createDate?: boolean;
+  updateDate?: boolean;
+  deleteDate?: boolean;
+  version?: boolean;
+  length?: string | number;
+  width?: number;
+  nullable?: boolean;
+  readonly?: boolean;
+  select?: boolean;
+  /**
+   * Specifies if column's value must be unique or not.
+   */
+  unique?: boolean;
+  /**
+   * Column comment.
+   */
+  comment?: string;
+  /**
+   * Default database value.
+   */
+  default?: any;
+  /**
+   * The precision for a decimal (exact numeric) column (applies only for decimal column), which is the maximum
+   * number of digits that are stored for the values.
+   */
+  precision?: number;
+  /**
+   * The scale for a decimal (exact numeric) column (applies only for decimal column), which represents the number
+   * of digits to the right of the decimal point and must not be greater than precision.
+   */
+  scale?: number;
 }
--- src/components/models-board/store/column.ts
+++ src/components/models-board/store/column.ts
@@ -8,13 +8,41 @@ export class ColumnStore{
   type: ColumnType;
   primary?: boolean;
   generated?: boolean;
-  
+  createDate?: boolean;
+  updateDate?: boolean;
+  deleteDate?: boolean;
+  version?: boolean;
+  length?: string | number;
+  width?: number;
+  nullable?: boolean;
+  readonly?: boolean;
+  select?: boolean;
+  unique?: boolean;
+  comment?: string;
+  default?: any;
+  precision?: number;
+  scale?: number;
+
   constructor(meta:ColumnMeta, public readonly entityStore: EntityStore){
     this.uuid = meta.uuid;
     this.name = meta.name;
     this.type = meta.type;
     this.primary = meta.primary;
     this.generated = meta.generated;
+    this.createDate = meta.createDate;
+    this.updateDate = meta.updateDate;
+    this.deleteDate = meta.deleteDate;
+    this.version = meta.version;
+    this.length = meta.length;
+    this.width = meta.width;
+    this.nullable = meta.nullable;
+    this.readonly = meta.readonly;
+    this.select = meta.select;
+    this.unique = meta.unique;
+    this.comment = meta.comment;
+    this.default = meta.default;
+    this.precision = meta.precision;
+    this.scale = meta.scale;
     makeAutoObservable(this)
   }
 
@@ -28,7 +56,21 @@ export class ColumnStore{
       name: this.name,
       type: this.type,
       primary: this.primary,
-      generated: this.generated
+      generated: this.generated,
+      createDate: this.createDate,
+      updateDate: this.updateDate,
+      deleteDate: this.deleteDate,
+      version: this.version,
+      length: this.length,
+      width: this.width,
+      nullable: this.nullable,
+      readonly: this.readonly,
+      select: this.select,
+      unique: this.unique,
+      comment: this.comment,
+      default: this.default,
+      precision: this.precision,
+      scale: this.scale,
     }
   }
 }
