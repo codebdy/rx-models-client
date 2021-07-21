b1f5eefd9322e8707a46805f0d81e4c4a0bce541
Water.Li <lizongbin@hotmail.com>
2021-07-21 22:33:54 +0800

    Update server-config.ts

--- src/data/server-config.ts
+++ src/data/server-config.ts
@@ -1,2 +1,2 @@
-export const serverUrl = "http://rxmodels.rxdrag.com";
-export const mediaServerUrl = "http://rxmodels.rxdrag.com";
+export const serverUrl = "https://rxmodels.rxdrag.com";
+export const mediaServerUrl = "https://rxmodels.rxdrag.com";
