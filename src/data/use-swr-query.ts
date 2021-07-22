21a61b9d2f3352bc6af838a8eb9a3694905131f3
Water.Li <lizongbin@hotmail.com>
2021-07-22 11:35:23 +0800

    Update use-swr-query.ts

--- src/data/use-swr-query.ts
+++ src/data/use-swr-query.ts
@@ -1,12 +1,14 @@
 import { AxiosRequestConfig } from "axios";
 import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import { useAppStore } from "store/app-store";
 import useSWR, { SWRResponse } from "swr";
-import { LOGIN_URL } from "util/consts";
+import { LOGIN_URL, TOKEN_NAME } from "util/consts";
 import { fetcher } from "./fetcher";
 
 export function useSWRQuery<T>(api?:AxiosRequestConfig, options?:any):SWRResponse<T, any>&{loading?:boolean}{
   const history = useHistory();
+  const appStore = useAppStore();
   const {onError, ...otherOptions} = options||{};
 
   const rtValue = useSWR<T>(api?.url||null, fetcher, {
@@ -21,9 +23,12 @@ export function useSWRQuery<T>(api?:AxiosRequestConfig, options?:any):SWRRespons
       onError(rtValue?.error);
     }
     if(rtValue?.error?.status === 401){
+      appStore.setToken('');
+      appStore.setLoggedUser(undefined);
+      localStorage.removeItem(TOKEN_NAME);
       history?.push(LOGIN_URL);
     }
-  },[rtValue.error, history, rtValue, onError])
-  const rtError = rtValue.error ? {message:rtValue.error?.message?.error || rtValue.error?.message} : undefined;
+  },[rtValue?.error, history, rtValue, onError, appStore])
+  const rtError = rtValue.error ? {message:rtValue.error?.message?.error || rtValue.error?.message?.message || rtValue.error?.message, status: rtValue?.error?.status} : undefined;
   return {...rtValue, loading: !rtValue.data && !rtValue.error && !!api?.url, error:rtError};
 }
