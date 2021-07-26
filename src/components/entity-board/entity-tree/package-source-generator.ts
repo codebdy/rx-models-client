8b9977730c9bb4939a2592fe47a49d2da5c58564
Water.Li <lizongbin@hotmail.com>
2021-07-26 20:52:25 +0800

    Update package-source-generator.ts

--- src/components/entity-board/entity-tree/package-source-generator.ts
+++ src/components/entity-board/entity-tree/package-source-generator.ts
@@ -64,6 +64,7 @@ export class PackageSourceGenerator{
       })
     source = source + _.uniq(sourceImports.concat(targetImports).concat(enumImports)).join('\n');
     source = source + ((sourceImports.length + targetImports.length) > 0 ? '\n\n' : '');
+    source = source + `export const Entity${entity.name} = '${entity.name}';\n\n`;
     source = source + `export interface ${entity.name} {\n`;
     source = source + entity.columns.map(column => {
       if (column.name === 'id') {
