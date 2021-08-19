9146791fc173023a85fe58c14a0b77227dac6bc1
Water.Li <lizongbin@hotmail.com>
2021-08-19 18:12:48 +0800

    Update package-source-generator.ts

--- src/components/entity-board/entity-tree/package-source-generator.ts
+++ src/components/entity-board/entity-tree/package-source-generator.ts
@@ -75,11 +75,11 @@ export class PackageSourceGenerator{
           .concat(enumImports)
           .concat(interfaceImports)
       )
-      .join('\n');
+      .join('\n') + '\n';
 
-    source = source + ((sourceImports.length + targetImports.length) > 0 ? '\n\n' : '');
+    source = source + ((sourceImports.length + targetImports.length) > 0 ? '\n' : '');
     if(entity.entityType !== EntityType.ENUM && entity.entityType !== EntityType.INTERFACE){
-      source = source + `export const Entity${entity.name} = '${entity.name}';\n\n`;      
+      source = source + `export const Entity${entity.name} = '${entity.name}';\n`;      
     }
 
     source = source + `export interface ${entity.name} {\n`;
