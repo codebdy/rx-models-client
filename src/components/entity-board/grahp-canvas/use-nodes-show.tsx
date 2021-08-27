b3bbf47b5475e027d874e8a5e5ad220b1b7919cd
Water.Li <lizongbin@hotmail.com>
2021-08-27 15:31:03 +0800

    fixed bug:https://github.com/rxdrag/rx-models/issues/18

--- src/components/entity-board/grahp-canvas/use-nodes-show.tsx
+++ src/components/entity-board/grahp-canvas/use-nodes-show.tsx
@@ -88,7 +88,7 @@ export function useNodesShow(){
       }
     })
     modelStore.graph?.getNodes().forEach(node=>{
-      if(!modelStore.getEntityById(node.id)){
+      if(!modelStore.openedDiagram?.getNodeById(node.id)){
         modelStore.graph?.removeNode(node.id);
       }
     })
