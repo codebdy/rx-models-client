bfdb4eeb7ce26da2e998f85c4252aa2e88d8be2a
Water.Li <lizongbin@hotmail.com>
2022-02-06 22:23:38 +0800

    install graphql requext

--- src/util/consts.ts
+++ src/util/consts.ts
@@ -5,3 +5,4 @@ export const INTALL_URL = "/install"
 export const INDEX_URL = "/studio";
 export const PRIMARY_COLOR = "#5d78ff";
 export const TREE_ROOT_ID = 'root';
+export const GRAPHQL_SERVER = "http://localhost:8080/graphql"
