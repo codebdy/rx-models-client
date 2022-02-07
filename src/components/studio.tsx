3cdff1a70a3b69d1dfc0d15d77db3bbd5e91b01f
Water.Li <lizongbin@hotmail.com>
2022-02-07 20:40:46 +0800

    Update studio.tsx

--- src/components/studio.tsx
+++ src/components/studio.tsx
@@ -4,7 +4,7 @@ import createStyles from "@mui/styles/createStyles";
 import makeStyles from "@mui/styles/makeStyles";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import { Avatar, IconButton, Link, Tooltip } from "@mui/material";
+import { Avatar, IconButton, Link, SvgIcon, Tooltip } from "@mui/material";
 import intl from "react-intl-universal";
 import { NavLink, Redirect, Route, Switch, useHistory } from "react-router-dom";
 import MdiIcon from "./common/mdi-icon";
@@ -28,7 +28,6 @@ const useStyles = makeStyles((theme: Theme) =>
       backgroundColor: theme.palette.primary.main,
       letterSpacing: "1px",
       fontWeight: "bold",
-      fontSize: "20px",
     },
     githubLink: {
       color: theme.palette.text.secondary,
@@ -71,7 +70,12 @@ export const Studio = () => {
       <AppBar position="static" color="transparent" variant="outlined">
         <Toolbar>
           <Avatar variant="rounded" className={classes.logoIcon}>
-            RX
+            <SvgIcon sx={{ fontSize: 32 }}>
+              <path
+                fill="currentColor"
+                d="M21,16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V7.5C3,7.12 3.21,6.79 3.53,6.62L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.79,6.79 21,7.12 21,7.5V16.5M12,4.15L6.04,7.5L12,10.85L17.96,7.5L12,4.15M5,15.91L11,19.29V12.58L5,9.21V15.91M19,15.91V9.21L13,12.58V19.29L19,15.91Z"
+              />
+            </SvgIcon>
           </Avatar>
           <NavLink
             className={classes.navLink}
