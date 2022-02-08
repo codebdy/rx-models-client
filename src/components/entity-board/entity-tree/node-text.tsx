6c12edbfde7eb98546b42e7684302abf45b78ebd
Water.Li <lizongbin@hotmail.com>
2022-02-08 15:11:32 +0800

    Update node-text.tsx

--- src/components/entity-board/entity-tree/node-text.tsx
+++ src/components/entity-board/entity-tree/node-text.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 
 export function NodeText(props: { children: any }) {
   return (
-    <Typography variant="body2" sx={{ pl: 1 }}>
+    <Typography variant="body2" component="div" sx={{ pl: 1 }}>
       {props.children}
     </Typography>
   );
