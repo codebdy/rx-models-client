9216af75f8bcfda4a9961b6e3a2c56fc3da6e84c
Water.Li <lizongbin@hotmail.com>
2022-02-08 11:04:22 +0800

    Update tree-node-label.tsx

--- src/components/entity-board/entity-tree/tree-node-label.tsx
+++ src/components/entity-board/entity-tree/tree-node-label.tsx
@@ -1,48 +1,42 @@
 import { Theme } from "@mui/material";
-import makeStyles from '@mui/styles/makeStyles';
-import createStyles from '@mui/styles/createStyles';
+import makeStyles from "@mui/styles/makeStyles";
+import createStyles from "@mui/styles/createStyles";
 import { useState } from "react";
 import { NodeAction } from "./node-action";
 
 const useStyles = makeStyles((theme: Theme) =>
-createStyles({
-  root: {
-    display:'flex',
-    alignItems: 'center',
-    padding:'5px 0',
-    position:'relative',
-    userSelect: 'none',
-  },  
-
-}),
+  createStyles({
+    root: {
+      display: "flex",
+      alignItems: "center",
+      padding: "5px 0",
+      position: "relative",
+      userSelect: "none",
+    },
+  })
 );
 
-export function TreeNodeLabel(props:{
-  children:any,
-  action?:any,
-  onClick?:(event:React.MouseEvent)=>void,
-  onDragStart?:(event: React.MouseEvent<HTMLDivElement, MouseEvent>)=>void
-}){
-  const {action, children, onClick, onDragStart} = props;
+export function TreeNodeLabel(props: {
+  children: any;
+  action?: any;
+  onClick?: (event: React.MouseEvent) => void;
+  onDragStart?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+}) {
+  const { action, children, onClick, onDragStart } = props;
   const classes = useStyles();
   const [hover, setHover] = useState(false);
 
-  return(
+  return (
     <div
       className={classes.root}
-      onMouseOver = {()=>setHover(true)}
-      onMouseLeave = {()=>setHover(false)}  
-      onClick = {onClick}
-      onDragStart = {onDragStart}
+      onMouseOver={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      onClick={onClick}
+      onDragStart={onDragStart}
       draggable
     >
       {children}
-      {
-        hover &&
-        <NodeAction>
-          {action}
-        </NodeAction>
-      }
-    </div>    
-  )
+      {hover && <NodeAction>{action}</NodeAction>}
+    </div>
+  );
 }
