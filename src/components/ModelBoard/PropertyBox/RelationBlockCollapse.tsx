24ed3c546ddcb6fa162a1b441ab5c1abf3685613
Water.Li <lizongbin@hotmail.com>
2022-03-29 17:22:58 +0800

    backup

---
+++ src/components/ModelBoard/PropertyBox/RelationBlockCollapse.tsx
@@ -0,0 +1,38 @@
+import { memo, useCallback, useState } from "react";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { Box, Collapse, Typography } from "@mui/material";
+
+export const RelationBlockCollapse = memo(
+  (props: { title: string; children: React.ReactNode }) => {
+    const { title, children } = props;
+    const [expanded, setExpanded] = useState(false);
+    const handleToggle = useCallback(() => {
+      setExpanded((a) => !a);
+    }, []);
+
+    return (
+      <>
+        <Box
+          sx={{
+            width: "100%",
+            cursor: "pointer",
+            display: "flex",
+            flexFlow: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            p: 1,
+            pl: 2,
+            zIndex: 1,
+          }}
+          onClick={handleToggle}
+        >
+          <Typography variant="subtitle1">{title}</Typography>
+          <ChevronRightIcon />
+        </Box>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          {children}
+        </Collapse>
+      </>
+    );
+  }
+);
--- src/components/ModelBoard/PropertyBox/RelationPanel.tsx
+++ src/components/ModelBoard/PropertyBox/RelationPanel.tsx
@@ -1,15 +1,12 @@
 import React, { useCallback, useMemo } from "react";
 import intl from "react-intl-universal";
 import {
-  Box,
-  Collapse,
   FormControl,
   Grid,
   InputLabel,
   MenuItem,
   Select,
   SelectChangeEvent,
-  Typography,
 } from "@mui/material";
 import LazyTextField from "components/ModelBoard/PropertyBox/LazyTextField";
 import {
@@ -20,19 +17,14 @@ import {
 import { useClass } from "../hooks/useClass";
 import { useChangeRelation } from "../hooks/useChangeRelation";
 import { useServiceId } from "../hooks/useServiceId";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { RelationBlockCollapse } from "./RelationBlockCollapse";
 
 export const RelationPanel = (props: { relation: RelationMeta }) => {
   const { relation } = props;
-  const [sourceExpanded, setSourceExpanded] = React.useState(false);
   const serviceId = useServiceId();
   const source = useClass(relation.sourceId, serviceId);
   const target = useClass(relation.targetId, serviceId);
   const changeRelation = useChangeRelation(serviceId);
-
-  const handleToggleSource = useCallback(() => {
-    setSourceExpanded((a) => !a);
-  }, []);
 
   const handleSourceMultiplicityChange = useCallback(
     (event: SelectChangeEvent<RelationMultiplicity>) => {
@@ -106,26 +98,7 @@ export const RelationPanel = (props: { relation: RelationMeta }) => {
         </Grid>
       ) : (
         <>
-          <Box
-            sx={{
-              width: "100%",
-              cursor: "pointer",
-              display: "flex",
-              flexFlow: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-              p: 1,
-              pl: 2,
-              zIndex: 1,
-            }}
-            onClick={handleToggleSource}
-          >
-            <Typography variant="subtitle1">
-              {source?.name} {intl.get("side")}
-            </Typography>
-            <ChevronRightIcon />
-          </Box>
-          <Collapse in={sourceExpanded} timeout="auto" unmountOnExit>
+          <RelationBlockCollapse title={source?.name + intl.get("side")}>
             <Grid container spacing={2} sx={{ pt: 2, pl: 2 }}>
               <Grid item xs={12}>
                 <LazyTextField
@@ -166,45 +139,44 @@ export const RelationPanel = (props: { relation: RelationMeta }) => {
                 />
               </Grid>
             </Grid>
-          </Collapse>
-          <Grid item xs={12}>
-            <Typography variant="subtitle1">
-              {target?.name} {intl.get("side")}
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <LazyTextField
-              label={intl.get("role-name")}
-              value={relation.roleOfSource || ""}
-              onChange={handleTargetRoleChange}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormControl variant="outlined" fullWidth size="small">
-              <InputLabel>{intl.get("multiplicity")}</InputLabel>
-              <Select
-                value={relation.targetMultiplicity || ""}
-                onChange={handleTargetMultiplicityChange}
-                label={intl.get("multiplicity")}
-              >
-                <MenuItem value={RelationMultiplicity.ZERO_ONE}>
-                  {RelationMultiplicity.ZERO_ONE}
-                </MenuItem>
-                <MenuItem value={RelationMultiplicity.ZERO_MANY}>
-                  {RelationMultiplicity.ZERO_MANY}
-                </MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-            <LazyTextField
-              label={intl.get("description")}
-              value={relation.descriptionOnTarget || ""}
-              multiline
-              rows={4}
-              onChange={handleTargetDescriptionChange}
-            />
-          </Grid>
+          </RelationBlockCollapse>
+          <RelationBlockCollapse title={target?.name + intl.get("side")}>
+            <Grid container spacing={2} sx={{ pt: 2, pl: 2 }}>
+              <Grid item xs={12}>
+                <LazyTextField
+                  label={intl.get("role-name")}
+                  value={relation.roleOfSource || ""}
+                  onChange={handleTargetRoleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl variant="outlined" fullWidth size="small">
+                  <InputLabel>{intl.get("multiplicity")}</InputLabel>
+                  <Select
+                    value={relation.targetMultiplicity || ""}
+                    onChange={handleTargetMultiplicityChange}
+                    label={intl.get("multiplicity")}
+                  >
+                    <MenuItem value={RelationMultiplicity.ZERO_ONE}>
+                      {RelationMultiplicity.ZERO_ONE}
+                    </MenuItem>
+                    <MenuItem value={RelationMultiplicity.ZERO_MANY}>
+                      {RelationMultiplicity.ZERO_MANY}
+                    </MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <LazyTextField
+                  label={intl.get("description")}
+                  value={relation.descriptionOnTarget || ""}
+                  multiline
+                  rows={4}
+                  onChange={handleTargetDescriptionChange}
+                />
+              </Grid>
+            </Grid>
+          </RelationBlockCollapse>
         </>
       )}
     </>
