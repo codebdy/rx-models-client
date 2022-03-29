3785a2277dbc43f50818500bace03ded616e5e3f
Water.Li <lizongbin@hotmail.com>
2022-03-29 20:17:53 +0800

    backup

--- src/components/ModelBoard/PropertyBox/FieldList.tsx
+++ src/components/ModelBoard/PropertyBox/FieldList.tsx
@@ -1,18 +1,51 @@
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid, IconButton, Typography } from "@mui/material";
 import { memo } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import intl from "react-intl-universal";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
 export const FieldList = memo(() => {
   return (
-    <Grid container item xs={12} spacing={2}>
+    <Grid container item xs={12} spacing={2} sx={{ pb: 4 }}>
       <Grid
         item
+        xs={12}
         sx={{
           display: "flex",
           justifyContent: "space-between",
+          alignItems: "center",
         }}
       >
-        <Typography sx={{ fontSize: "0.9rem" }}>字段列表</Typography>
+        <Typography sx={{ fontSize: "0.9rem" }}>
+          {intl.get("field-list")}
+        </Typography>
+        <IconButton size="small">
+          <AddIcon fontSize="small" />
+        </IconButton>
       </Grid>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          background: "red",
+          ml: 1,
+          mr: 1,
+          pl: 1,
+        }}
+      >
+        <Typography>alt:String</Typography>
+        <Box sx={{ display: "flex" }}>
+          <IconButton size="small">
+            <ModeEditIcon fontSize="small" />
+          </IconButton>
+          <IconButton size="small">
+            <DeleteOutlineIcon fontSize="small" />
+          </IconButton>
+        </Box>
+      </Box>
     </Grid>
   );
 });
