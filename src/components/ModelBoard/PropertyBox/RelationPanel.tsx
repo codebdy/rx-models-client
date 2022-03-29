84b8c2c08d33876d162f349ed6e1434a3efe8f05
Water.Li <lizongbin@hotmail.com>
2022-03-29 17:13:39 +0800

    Update RelationPanel.tsx

--- src/components/ModelBoard/PropertyBox/RelationPanel.tsx
+++ src/components/ModelBoard/PropertyBox/RelationPanel.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useMemo } from "react";
 import intl from "react-intl-universal";
 import {
+  Box,
+  Collapse,
   FormControl,
   Grid,
   InputLabel,
@@ -18,13 +20,19 @@ import {
 import { useClass } from "../hooks/useClass";
 import { useChangeRelation } from "../hooks/useChangeRelation";
 import { useServiceId } from "../hooks/useServiceId";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 export const RelationPanel = (props: { relation: RelationMeta }) => {
   const { relation } = props;
+  const [sourceExpanded, setSourceExpanded] = React.useState(false);
   const serviceId = useServiceId();
   const source = useClass(relation.sourceId, serviceId);
   const target = useClass(relation.targetId, serviceId);
   const changeRelation = useChangeRelation(serviceId);
+
+  const handleToggleSource = useCallback(() => {
+    setSourceExpanded((a) => !a);
+  }, []);
 
   const handleSourceMultiplicityChange = useCallback(
     (event: SelectChangeEvent<RelationMultiplicity>) => {
@@ -92,50 +100,73 @@ export const RelationPanel = (props: { relation: RelationMeta }) => {
 
   return (
     <>
-      {!isInherit && (
+      {isInherit ? (
+        <Grid item xs={12}>
+          {intl.get("inherit")}
+        </Grid>
+      ) : (
         <>
-          <Grid item xs={12}>
+          <Box
+            sx={{
+              width: "100%",
+              cursor: "pointer",
+              display: "flex",
+              flexFlow: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              p: 1,
+              pl: 2,
+              zIndex: 1,
+            }}
+            onClick={handleToggleSource}
+          >
             <Typography variant="subtitle1">
               {source?.name} {intl.get("side")}
             </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <LazyTextField
-              label={intl.get("role-name")}
-              value={relation.roleOfTarget || ""}
-              onChange={handleSourceRoleChange}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormControl variant="outlined" fullWidth size="small">
-              <InputLabel>{intl.get("multiplicity")}</InputLabel>
-              <Select
-                value={relation.sourceMutiplicity || ""}
-                onChange={handleSourceMultiplicityChange}
-                label={intl.get("multiplicity")}
-              >
-                <MenuItem value={RelationMultiplicity.ZERO_ONE}>
-                  {RelationMultiplicity.ZERO_ONE}
-                </MenuItem>
-                {relation.relationType !== RelationType.ONE_WAY_COMBINATION &&
-                  relation.relationType !==
-                    RelationType.TWO_WAY_COMBINATION && (
-                    <MenuItem value={RelationMultiplicity.ZERO_MANY}>
-                      {RelationMultiplicity.ZERO_MANY}
+            <ChevronRightIcon />
+          </Box>
+          <Collapse in={sourceExpanded} timeout="auto" unmountOnExit>
+            <Grid container spacing={2} sx={{ pt: 2, pl: 2 }}>
+              <Grid item xs={12}>
+                <LazyTextField
+                  label={intl.get("role-name")}
+                  value={relation.roleOfTarget || ""}
+                  onChange={handleSourceRoleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl variant="outlined" fullWidth size="small">
+                  <InputLabel>{intl.get("multiplicity")}</InputLabel>
+                  <Select
+                    value={relation.sourceMutiplicity || ""}
+                    onChange={handleSourceMultiplicityChange}
+                    label={intl.get("multiplicity")}
+                  >
+                    <MenuItem value={RelationMultiplicity.ZERO_ONE}>
+                      {RelationMultiplicity.ZERO_ONE}
                     </MenuItem>
-                  )}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-            <LazyTextField
-              label={intl.get("description")}
-              value={relation.descriptionOnSource || ""}
-              multiline
-              rows={4}
-              onChange={handleSourceDescriptionChange}
-            />
-          </Grid>
+                    {relation.relationType !==
+                      RelationType.ONE_WAY_COMBINATION &&
+                      relation.relationType !==
+                        RelationType.TWO_WAY_COMBINATION && (
+                        <MenuItem value={RelationMultiplicity.ZERO_MANY}>
+                          {RelationMultiplicity.ZERO_MANY}
+                        </MenuItem>
+                      )}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <LazyTextField
+                  label={intl.get("description")}
+                  value={relation.descriptionOnSource || ""}
+                  multiline
+                  rows={4}
+                  onChange={handleSourceDescriptionChange}
+                />
+              </Grid>
+            </Grid>
+          </Collapse>
           <Grid item xs={12}>
             <Typography variant="subtitle1">
               {target?.name} {intl.get("side")}
